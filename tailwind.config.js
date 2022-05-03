4cbf5a24002c02f0d1d0fb31ca248f66bf75bfca
Denver Danniel <dnvrdnnl09@gmail.com>
2022-05-03 11:08:53 +0800

    reponsiveness complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,8 +13,11 @@ module.exports = {
     screens: {
       'screen250px' : {'max' : '250px'},
       'screen420px' : {'max' : '420px'},
+      'screen500px' : {'max' : '500px'},
+      'screen550px' : {'max' : '550px'},
       'screen600px' : {'max' : '600px'},
       'screen650px' : {'max' : '650px'},
+      'screen800px' : {'max' : '800px'},
       'screen1000px' : {'max' : '1000px'},
       'screen1020px' : {'max' : '1020px'},
     }
