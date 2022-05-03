8db12425f62c8dde38ebf364217561158ee9e0f1
Denver Danniel <dnvrdnnl09@gmail.com>
2022-05-03 18:33:53 +0800

    add back to top button

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Footer from './Footer'
+import BackTopTop from './BackToTop'
 
 const Layout = ({ children }) => {
+
+  const [showToTop, setShowToTop] = useState(false)
+
+  useEffect(() => {
+    window.onscroll = () => {
+      let scroll =  window.pageYOffset
+
+      if(scroll > 0) {
+        setShowToTop(true)
+      }
+      else{
+        setShowToTop(false)
+      }
+    }
+  }, [])
+  
+
   return (
     <div className='w-full h-auto screen250px:w-[250px]'>
         { children }
         <Footer/>
+        <BackTopTop showToTop={showToTop}/>
     </div>
   )
 }
